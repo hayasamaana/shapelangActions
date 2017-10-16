f92505b2d511e712877efbce50ee6a927d4bda0a
najah <najah@172.16.74.25>
2017-10-16 12:20:50 +0300

    register meta model

---
+++ src/shapelang/actions/handlers/RegisterMetaModeel.java
@@ -0,0 +1,11 @@
+package shapelang.actions.handlers;
+
+import java.io.File;
+
+public class RegisterMetaModeel {
+
+	public RegisterMetaModeel(File file) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
